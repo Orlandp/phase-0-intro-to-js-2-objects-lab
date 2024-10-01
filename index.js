9dfaf27781709aa746e879b48cd8aa657af849e2
pknowledge <jamesblssings@gmail.com>
2024-10-01 06:09:20 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+// Create an employee object
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St"
+};
+
+// Function 1: Non-destructive update
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+// Function 2: Destructive update
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+// Function 3: Non-destructive delete
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+// Function 4: Destructive delete
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
